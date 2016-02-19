a86e800b74987e8cf949cac1a47570e53a348af3
jujupowaa <j.leleu@live.fr>
2016-02-19 18:20:57 +0100

    "modif mineure"

--- src/fr/da2i/tache/entity/Client.java
+++ src/fr/da2i/tache/entity/Client.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
 	
@@ -60,12 +61,44 @@ public class Client {
 		}
 	    return null;
 	}
+	
+	public int getChoice() {
+		System.out.println("Connecté avec " + login);
+		System.out.println("1. Informations sur une tâche");
+		System.out.println("2. Créer une tâche");
+		System.out.println("3. Mettre à jour une tâche");
+		System.out.println("4. Supprimer une tâche");
+		System.out.println("5. Créer un utilisateur");
+		System.out.println("Votre choix ?");
+		
+		Scanner sc = new Scanner(System.in);
+		return Integer.parseInt(sc.nextLine());
+	}
 
 	public static void main(String[] args) {
 		//Client client = new Client(args[0], Integer.parseInt(args[1]), args[2]);
 		Client client = new Client("localhost", 9876, "julien");
+		String request = "";
+		switch(client.getChoice()) {
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;
+		case 5:
+			
+			break;
+		default:
+			System.out.println("Choix incorrect");
+		}
 		String receipt = client.send("GET ");
 		System.out.println(receipt);
-		System.out.println("Client");
 	}
 }
